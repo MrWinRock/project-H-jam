345c5aeb586bfcccea4576334a391379678b09d4
ratchakit <basszamama20@gmail.com>
2024-10-15 00:59:27 +0700

    Add: Script(AcceptTicket, DestoryTicket, RejectButton) Prefab:(ClockInMachine, Ticket, TicketClockIn)

---
+++ Assets/Scripts/DayGameplay/AcceptTicket.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AcceptTicket : MonoBehaviour
+{
+    public Vector3 newPosition;
+    public Vector3 oldPosition;          // New position to set
+    private float currentTime;
+    public float countdownTime;
+    public GameObject objectToMove;
+    public GameObject ticketClockIn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = countdownTime;  // Initialize the timer with the 6-minute countdown
+    }
+
+    void Update()
+    {
+        GameObject foundObject = GameObject.FindWithTag("TicketClockIn");
+        if (foundObject != null)
+        {
+            currentTime -= Time.deltaTime;
+            Console.WriteLine(currentTime);
+            if (currentTime <= 0)
+            {
+                objectToMove.transform.position = oldPosition;
+            }
+        }
+        else
+        {
+            Debug.Log("Object with tag exist in the scene.");
+        }
+}
+        private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("TicketClockIn"))
+        {
+            currentTime = 15f;
+
+            // Check if the object to move is assigned
+            if (objectToMove != null)
+            {
+                // Change the position of the object
+                objectToMove.transform.position = newPosition;
+            }
+        }
+    }
+}
--- Assets/Scripts/DayGameplay/RejectButton.cs
+++ Assets/Scripts/DayGameplay/RejectButton.cs
@@ -3,34 +3,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RejectButton : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class RejectButton : MonoBehaviour {
+    public GameObject objectToActivate;   // Object to activate
+    public GameObject objectToDestroy;    // Object to destroy  // Script to enable
+    public GameObject objectToMove;
+    public GameObject ticketClockIn;// Object to move
+    public Vector3 newPosition;
+    public Vector3 oldPosition;
+    public float countdownTime;           // New position to set
+    private float currentTime;
+
     void Start()
     {
-        
+        currentTime = countdownTime;  // Initialize the timer with the 6-minute countdown
     }
-
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    // This method is called when the object is clicked (when the mouse button is released over the object)
-    void OnMouseUp()
-    {
-        Debug.Log("You are clicked!"); // This will display in the Unity console
+        GameObject foundObject = GameObject.FindWithTag("TicketClockIn");
+        if (foundObject == null)
+        {
+            currentTime -= Time.deltaTime;
+            Console.WriteLine(currentTime);
+            if (currentTime <= 0)
+            {
+                objectToMove.transform.position = oldPosition;
+            }
+        }
+        else
+        {
+            Debug.Log("Object with tag does not exist in the scene.");
+        }
     }
 
-    // This method is called when the mouse is over the object and pressed down
-    void OnMouseDown()
+    // This method is called when the mouse button is pressed over the object
+    private void OnMouseDown()
     {
-        Debug.Log("Mouse button is pressed on the object.");
+        currentTime = 15f;
+        Debug.Log("Mouse Down on RejectButton!");
+
+        // Check if the object to move is assigned
+        if (objectToMove != null)
+        {
+            // Change the position of the object
+            objectToMove.transform.position = newPosition;
+            Debug.Log(objectToMove.name + " moved to: " + newPosition);
+
+        }
+        if (objectToDestroy.scene.isLoaded) // Check if the GameObject is in a loaded scene
+        {
+            Destroy(objectToDestroy);
+        }
+
     }
 
-    // This method is called when the mouse hovers over the object (optional)
-    void OnMouseOver()
+    // Optional: This method is called when the mouse hovers over the object
+    private void OnMouseOver()
     {
         Debug.Log("Mouse is over the object.");
+    }
+
+    // Optional: This method is called when the mouse button is released over the object
+    private void OnMouseUp()
+    {
+        Debug.Log("You clicked and released the mouse!");
     }
 }

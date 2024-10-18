afc5256dd6f89e08270d678af0b189cef5946fc7
Marguro <108455691+Marguro@users.noreply.github.com>
2024-10-18 14:59:28 +0700

    update: SFX in game

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    [Header("----------Audio Sources----------")]
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource SFXSource;
-    
+
+    [Header("----------Audio Clip----------")]
     public AudioClip backgroundMusic;
-    
+    public AudioClip cardClick;
+    public AudioClip mrPo;
+    public AudioClip checker;
+    public AudioClip cardSend;
+    private bool hasPlayedTicket = false;
+    private bool hasPlayedMainCard = false;
+    private bool hasPlayedLpd = false;
+
     private void Start()
     {
         musicSource.clip = backgroundMusic;
@@ -19,6 +28,42 @@ public class AudioManager : MonoBehaviour
         if (!musicSource.isPlaying)
         {
             musicSource.Play();
+        }
+
+        GameObject foundObject1 = GameObject.FindWithTag("Ticket");
+        if (foundObject1 != null && !hasPlayedTicket)
+        {
+            SFXSource.PlayOneShot(cardSend);
+            hasPlayedTicket = true;
+        }
+
+        if (foundObject1 == null)
+        {
+            hasPlayedTicket = false;
+        }
+        
+        GameObject foundObject2 = GameObject.FindWithTag("MainCard");
+        if (foundObject2 != null && !hasPlayedMainCard)
+        {
+            SFXSource.PlayOneShot(cardClick);
+            hasPlayedMainCard = true;
+        }
+
+        if (foundObject2 == null)
+        {
+            hasPlayedMainCard = false;
+        }
+        
+        GameObject foundObject3 = GameObject.FindWithTag("Lpd");
+        if (foundObject3 != null && !hasPlayedLpd)
+        {
+            SFXSource.PlayOneShot(mrPo);
+            hasPlayedLpd = true;
+        }
+
+        if (foundObject3 == null)
+        {
+            hasPlayedLpd = false;
         }
     }
 }
